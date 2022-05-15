136728eb1003fc0eed468fd5c292760cd1be6223
SeverinPeterHSLU <71264007+SeverinPeterHSLU@users.noreply.github.com>
2022-05-15 15:49:39 +0200

    Will set up DB now

--- src/main/java/ch/hslu/sweng/group3/App.java
+++ src/main/java/ch/hslu/sweng/group3/App.java
@@ -1,10 +1,10 @@
 package ch.hslu.sweng.group3;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.DriverManager;
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.Properties;
 
 public class App {
@@ -12,6 +12,35 @@ public class App {
     protected static Connection db;
     private static final String configLocation = "src/config.properties";
 
+    private static boolean hasNoTables() {
+        String sql = "SELECT name FROM sqlite_schema\n" +
+                "WHERE type='table'\n" +
+                "ORDER BY name;";
+
+        try {
+            Statement stmnt = db.createStatement();
+            ResultSet res = stmnt.executeQuery(sql);
+            if (!res.next()) {
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private static void setUpDB() {
+        try {
+            Statement stmnt = db.createStatement();
+            stmnt.execute(SQL.movieTable);
+            stmnt.execute(SQL.roomTable);
+            stmnt.execute(SQL.customerTable);
+            stmnt.execute(SQL.showTable);
+            stmnt.execute(SQL.reservationTable);
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         MainFrame mainframe = new MainFrame();
@@ -25,6 +54,11 @@ public class App {
         try {
             db = DriverManager.getConnection(properties.getProperty("DB_URL"));
             System.out.println("got connected");
+            if (hasNoTables()) {
+                System.out.println("DB has no Tables!");
+                setUpDB();
+                System.out.println(hasNoTables());
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
