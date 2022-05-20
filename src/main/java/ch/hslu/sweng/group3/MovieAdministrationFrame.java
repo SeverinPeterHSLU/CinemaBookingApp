464a8caa9e55eff39bc196e875f7e0818a26e80d
janaomci <jana.omcikus@stud.hslu.ch>
2022-05-20 22:48:11 +0200

    MovieAdministration works

--- src/main/java/ch/hslu/sweng/group3/MovieAdministrationFrame.java
+++ src/main/java/ch/hslu/sweng/group3/MovieAdministrationFrame.java
@@ -1,9 +1,14 @@
 package ch.hslu.sweng.group3;
 
+
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
 public class MovieAdministrationFrame extends JFrame {
     private JTable activeMoviesTable;
@@ -11,14 +16,66 @@ public class MovieAdministrationFrame extends JFrame {
     private JButton btnAddMovie;
     private JButton btnBackToMain;
     private JPanel movieAdministrationPanel;
+    private JPanel movieTablePanel;
+
 
     public MovieAdministrationFrame() {
+        setLayout(null);
         setTitle("Movie Administration");
         setSize(1500, 800);
-        setLocationRelativeTo(null);
         setContentPane(movieAdministrationPanel);
+        movieTablePanel.setLayout(new BorderLayout());
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
+
+        DefaultTableModel model = new DefaultTableModel();
+        activeMoviesTable = new JTable(model);
+        movieTablePanel.add(activeMoviesTable, BorderLayout.CENTER);
+        movieTablePanel.add(new JScrollPane(activeMoviesTable));
+        activeMoviesTable.setModel(model);
+
+
+        Object[] headers = {"Movie ID", "Movie Title", "Duration (min)", "Edit", "Delete"};
+        model.setColumnIdentifiers(headers);
+
+
+        ArrayList<Movie> allActiveMovies = Movie.getMovies();
+
+
+        for (int r = 0; r < allActiveMovies.size(); r++) {
+            String movieID = Integer.toString(allActiveMovies.get(r).getMovieID());
+            String movieTitle = allActiveMovies.get(r).getMovieTitle();
+            String duration = Integer.toString(allActiveMovies.get(r).getMovieDuration());
+
+            Object[] row = {movieID, movieTitle, duration, "Edit", "Delete"};
+            model.addRow(row);
+            //model.setValueAt(new JButton("Edit"), r, 3);
+        }
+        Action edit = new AbstractAction() {
+            public void actionPerformed(ActionEvent e) {
+                Object obj = activeMoviesTable.getValueAt(activeMoviesTable.getSelectedRow(), 0);
+                String movieID_string = obj.toString();
+                int movieID = Integer.parseInt(movieID_string);
+                dispose();
+                EditMovieFormFrame editMovieFormFrame = new EditMovieFormFrame(movieID);
+            }
+        };
+        Action delete = new AbstractAction() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("hello");
+                Object obj = activeMoviesTable.getValueAt(activeMoviesTable.getSelectedRow(), 0);
+                String movieID_string = obj.toString();
+                int movieID = Integer.parseInt(movieID_string);
+                Movie m = Movie.getMovie(movieID);
+                Movie.removeMovie(m);
+                dispose();
+                MovieAdministrationFrame newMovieAdministrationFrame = new MovieAdministrationFrame();
+            }
+        };
+
+        ButtonColumn editButtons = new ButtonColumn(activeMoviesTable, edit, 3);
+        ButtonColumn deleteButtons = new ButtonColumn(activeMoviesTable, delete, 4);
+
 
         //opens form to create new movie
         btnAddMovie.addActionListener(new ActionListener() {
@@ -62,13 +119,16 @@ public class MovieAdministrationFrame extends JFrame {
         lblActiveMoviesTabel = new JLabel();
         lblActiveMoviesTabel.setText("Currently Screened Movies");
         movieAdministrationPanel.add(lblActiveMoviesTabel, new com.intellij.uiDesigner.core.GridConstraints(0, 0, 1, 2, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        activeMoviesTable = new JTable();
-        movieAdministrationPanel.add(activeMoviesTable, new com.intellij.uiDesigner.core.GridConstraints(1, 0, 1, 2, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_BOTH, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, null, new Dimension(150, 50), null, 0, false));
         btnAddMovie = new JButton();
         btnAddMovie.setText("Add Movie");
         movieAdministrationPanel.add(btnAddMovie, new com.intellij.uiDesigner.core.GridConstraints(2, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(100, 40), null, 0, false));
         btnBackToMain = new JButton();
         btnBackToMain.setText("Go Back");
         movieAdministrationPanel.add(btnBackToMain, new com.intellij.uiDesigner.core.GridConstraints(2, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(100, 40), null, 0, false));
+        movieTablePanel = new JPanel();
+        movieTablePanel.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
+        movieAdministrationPanel.add(movieTablePanel, new com.intellij.uiDesigner.core.GridConstraints(1, 0, 1, 2, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        activeMoviesTable = new JTable();
+        movieTablePanel.add(activeMoviesTable, new com.intellij.uiDesigner.core.GridConstraints(0, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, 50), null, 0, false));
     }
 }
