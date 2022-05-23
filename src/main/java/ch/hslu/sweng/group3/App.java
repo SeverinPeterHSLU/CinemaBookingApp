740ae4a34194c7326aede4435ab3cbab0b5e82a8
janaomci <jana.omcikus@stud.hslu.ch>
2022-05-24 00:40:50 +0200

    upload all

--- src/main/java/ch/hslu/sweng/group3/App.java
+++ src/main/java/ch/hslu/sweng/group3/App.java
@@ -44,14 +44,6 @@ public class App {
 
     public static void main(String[] args) {
 
-        Properties prop = new Properties();
-        prop.put("mail.smtp.host", "smtp.gmail.com");
-        prop.put("mail.smtp.port", "465");
-        prop.put("mail.smtp.auth", "true");
-        prop.put("mail.smtp.socketFactory.port", "465");
-        prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-
-
         try {
             db = DriverManager.getConnection("jdbc:sqlite:DataBase.db");
             System.out.println("got connected");
